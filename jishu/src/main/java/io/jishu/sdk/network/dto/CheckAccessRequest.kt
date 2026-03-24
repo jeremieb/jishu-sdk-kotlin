6ceb24b863a4ab79371032806cd2636a7f8d9f04
Jeremie Berduck <jeremieberduck@gmail.com>
2026-03-24 20:37:41 +0100

    Initial version

--- jishu/src/main/java/io/jishu/sdk/network/dto/CheckAccessRequest.kt
+++ jishu/src/main/java/io/jishu/sdk/network/dto/CheckAccessRequest.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 internal data class CheckAccessRequest(
     val appId: String,
-    val platform: String,
+    val platform: String = "android",
     val externalUserId: String? = null,
     val deviceId: String? = null,
     val environment: String? = null
